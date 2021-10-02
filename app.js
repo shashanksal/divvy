c6c698cb8ea6f3c5bbf8669942c334b1990e954d
Shashank Salunkhe <shashank.salunkhe@itinnovate.com.au>
2021-10-02 21:21:27 +1000

    chore: remove sass middleware

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-let indexRouter = require('./routes/index');
-let applicationRouter = require('./routes/application');
+let indexRouter = require("./routes/index");
+let applicationRouter = require("./routes/application");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/app/',applicationRouter)
+app.use("/", indexRouter);
+app.use("/app/", applicationRouter);
 
 module.exports = app;
