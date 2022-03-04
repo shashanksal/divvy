c2050d9a005588c49d20f0454e93853817ef5772
Shashank Salunkhe <shashank.sal047@gmail.com>
2022-03-04 23:01:17 +1100

    chore: add eslint and run linter

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-let indexRouter = require("./routes/index");
-let applicationRouter = require("./routes/application");
+let indexRouter = require('./routes/index');
+let applicationRouter = require('./routes/application');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/app/", applicationRouter);
+app.use('/', indexRouter);
+app.use('/app/', applicationRouter);
 
 module.exports = app;
--- models/Trips.js
+++ models/Trips.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let tripSchema = new Schema({
-    trip_id:{ type: String, unique: true },
-    start_time:{ type: String },
-    end_time:{type:String},
-    bikeid:{type:Number},
-    tripduration:{type:Number},
-    from_station_id:{type:Number},
-    from_station_name:{type:String},
-    to_station_id:{type:Number},
-    to_station_name:{type:String},
-    gender:{type:String},
-    birthyear:{type:Number , default:0}
+	trip_id:{ type: String, unique: true },
+	start_time:{ type: String },
+	end_time:{type:String},
+	bikeid:{type:Number},
+	tripduration:{type:Number},
+	from_station_id:{type:Number},
+	from_station_name:{type:String},
+	to_station_id:{type:Number},
+	to_station_name:{type:String},
+	gender:{type:String},
+	birthyear:{type:Number , default:0}
 });
 
 // tripSchema.pre('save', (next) => { // arrow function
